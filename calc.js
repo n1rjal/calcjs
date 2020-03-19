3109536f9507a333e10e628f1f1504ef7a288db8
n1rjal <nirjalpaudel54312@gmail.com>
2020-03-19 16:16:19 +0545

    backspace added

--- calc.js
+++ calc.js
@@ -5,25 +5,41 @@ let history=[];
 
 function writeinscreen(x){
     
-    console.log("called")
+    
     x=String(x);
     if (x!="AC" && x!="="){
         document.getElementById("screen").innerHTML+=x;
-        console.log(x);
+       
     }
     if (x=="AC"){
         document.getElementById("screen").innerHTML="";
-        console.log("Clearing");
+       
     }
+
+    if (x=="b"){
+        let tempcont="";
+        let content=document.getElementById("screen").innerText;
+        if (content.length!=0){
+            for (let i=0;i<(content.length-2);i++){
+                tempcont+=content.charAt(i);
+                console.log(content.charAt(i))    
+            }
+        }
+        console.log(tempcont);
+        document.getElementById("screen").innerHTML=tempcont;           
+        tempcont="";
+    }
+
     if (x=="="){
 
         try{
             let content=document.getElementById("screen").innerText;
-            history.push(String(content));
+            let content_to_put=document.getElementById("screen").innerText;
             content=content.replace("x","*");
             console.log(content);
             
             let processedcontent=eval(content);
+            history.push(content_to_put);
             document.getElementById("screen").innerHTML+=" = "+processedcontent;
 
             setTimeout(function(){
@@ -64,7 +80,5 @@ function showhistory(x){
 
         document.getElementById("screen").innerHTML=history[pointer];
     }
-    setTimeout(() => {
-        document.getElementById("screen").innerHTML=""
-    }, 3000);
+    
 }
